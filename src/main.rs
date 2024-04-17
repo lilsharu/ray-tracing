cbbfa23222009a00236e7cf4bfbddfb21d0eae15
Shourya Bansal <shouryabansal2012@gmail.com>
2024-04-17 19:37:40 -0400

    Add a Progress Bar

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use indicatif::ProgressBar;
+
 mod color;
 
 fn main() {
     // Image
-    let image_width = 256;
-    let image_height = 256;
+    let image_width = 256u32;
+    let image_height = 256u32;
 
     // Render
     println!("P3");
     println!("{image_width} {image_height}");
     println!("255");
 
+    let bar = ProgressBar::new(u64::from(image_width * image_height));
+    bar.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .template("Pixels Rendered: {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .unwrap()
+            .progress_chars("#>-"),
+    );
+
     for i in 0..image_height {
         for j in 0..image_width {
+            bar.inc(1);
             let pixel = {
                 let r = f64::from(i) / f64::from(image_width - 1);
                 let g = f64::from(j) / f64::from(image_height - 1);
@@ -27,4 +38,5 @@ fn main() {
             println!("{}", ppm_pixel.to_ppm_pixel());
         }
     }
+    bar.finish();
 }
